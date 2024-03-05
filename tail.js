8b2939b79a9e2481d9e15d0366e93e7cb7133976
animward <142351228+animward@users.noreply.github.com>
2024-03-04 17:00:25 -0800

    Update tail.js
    had issues with commit, pasted code from codespace

--- tail.js
+++ tail.js
@@ -1,1 +1,20 @@
+// Import assertEqual function
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
 
+  // Define head
+const tail = function(array) {
+    if (array.length <= 1) {
+        return [];
+    }
+    return array.slice(1);
+};
+
+// New cases
+assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
+assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[0], "Lighthouse");
