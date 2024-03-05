7fd99875296db323dd7adaf3d7fe0eec79d2a1a5
animward <142351228+animward@users.noreply.github.com>
2024-03-05 00:28:45 +0000

    head function

--- head.js
+++ head.js
@@ -1,1 +1,21 @@
 
+// Import assertEqual function
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+
+  // Define head
+const head = function(array) {
+    if (array.length === 0) {
+        return undefined;
+    }
+    return array[0];
+}
+
+// New cases
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
