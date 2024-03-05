eafba8a5113e6d03629aac1ff27561f22125971f
animward <142351228+animward@users.noreply.github.com>
2024-03-04 17:05:31 -0800

    Update tail.js

--- tail.js
+++ tail.js
@@ -18,3 +18,9 @@ const tail = function(array) {
 // New cases
 assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
 assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[0], "Lighthouse");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+assertEqual(tail(["Solo"]).length, 0);
