8add405732174830cd69faaa9d8ffa1549fa3133
animward <142351228+animward@users.noreply.github.com>
2024-03-07 21:12:14 +0000

    removed old test cases

--- map.js
+++ map.js
@@ -24,9 +24,4 @@ const eqArrays = function (arr1, arr2) {
     return true;
 };
 
-const array1 = [1, 2, 3];
-const array2 = [1, 2, 3];
-const array3 = [3, 2, 1];
-
-assertArraysEqual(array1, array2); // Should pass
-assertArraysEqual(array1, array3); // Should fail
+//map scenarios
