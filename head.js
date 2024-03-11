9b950b39e3da6d1c7bfe5399dc71f723f0bbb704
EgoExperiments <egoexperiments@gmail.com>
2024-03-11 12:43:08 -0700

    assertEqual test

--- head.js
+++ head.js
@@ -1,16 +1,10 @@
 
 // Import assertEqual function
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = require('./assertEqual');
 
-  // Define head
-const head = function(array) {
-    return array[0];
+// Define head
+const head = function (array) {
+  return array[0];
 }
 
 // New cases
