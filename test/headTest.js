a2915f4dc082124171c1788d38ce1c4a2be02b11
animward <142351228+animward@users.noreply.github.com>
2024-03-11 15:16:06 -0700

    test cases

--- test/headTest.js
+++ test/headTest.js
@@ -12,5 +12,12 @@ describe("#head", () => {
 });
 
 // Test cases
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe('head', function() {
+  it('should return 5 for [5, 6, 7]', function() {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it('should return "Hello" for ["Hello", "Lighthouse", "Labs"]', function() {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
