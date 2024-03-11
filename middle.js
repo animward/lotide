0d44632efefc8d2c0e21c9b587ead8463d9c8827
EgoExperiments <egoexperiments@gmail.com>
2024-03-11 14:20:29 -0700

    imported middle.js

---
+++ middle.js
@@ -0,0 +1,45 @@
+// TEST/ASSERTION FUNCTIONS
+const eqArrays = function (arr1, arr2) {
+  // Check if the arrays have the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check each element for equality
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// ACTUAL FUNCTION
+const middle = function (array) {
+  const length = array.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length % 2 === 1) {
+    return [array[middleIndex]];
+  } else {
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+}
+
+// TEST CODE
+// Test cases for various scenarios
+assertArraysEqual(middle([1]), []); // Should pass
+assertArraysEqual(middle([1, 2]), []); // Should pass
+assertArraysEqual(middle([1, 2, 3]), [2]); // Should pass
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Should pass
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Should pass
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Should pass
