ec4df4b75a4a3fa9b9c144715bc6d2a33abe6c67
EgoExperiments <egoexperiments@gmail.com>
2024-03-11 12:50:14 -0700

    head tests

--- head.js
+++ head.js
@@ -7,6 +7,8 @@ const head = function (array) {
   return array[0];
 }
 
+module.exports = head;
+
 // New cases
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
