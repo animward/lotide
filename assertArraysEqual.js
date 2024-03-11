03944dfb3bf3d172975164af77c7ce2a30c36239
EgoExperiments <egoexperiments@gmail.com>
2024-03-11 13:55:21 -0700

    re defined assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,18 +3,18 @@ const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 
-
-// Check each element for equality
-for (let i = 0; i < arr1.length; i++) {
-  if (arr1[i] !== arr2[i]) {
-    return false;
-    // Use break to exit the loop when an unequal pair is found
-    break;
+const assertArraysEqual = function (arr1, arr2) {
+  // Check each element for equality
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+      // Use break to exit the loop when an unequal pair is found
+      break;
+    }
   }
+
+  return true;
 }
-
-return true;
-
 
 const array1 = [1, 2, 3];
 const array2 = [1, 2, 3];
