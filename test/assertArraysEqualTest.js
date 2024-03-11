d4fb6c5f2d98c1593ca8c54e4226cbe49cbfa208
EgoExperiments <egoexperiments@gmail.com>
2024-03-11 13:47:28 -0700

    removed assertEqual from test

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,4 @@
 const assertArraysEqual = require('../assertArraysEqual');
-const assertEqual = require('../assertEqual');
 
 assertArraysEqual(array1, array2); // Should pass
 assertArraysEqual(array1, array3); // Should fail
