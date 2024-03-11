e9704f85bfcd9f132c60c4e2cb3d347764785f64
EgoExperiments <egoexperiments@gmail.com>
2024-03-11 15:26:03 -0700

    chai conversion

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,16 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []); // Should pass
-assertArraysEqual(middle([1, 2]), []); // Should pass
-assertArraysEqual(middle([1, 2, 3]), [2]); // Should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Should pass
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Should pass
+describe('middle', function() {
+  it('should return [2] for [1, 2, 3]', function() {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('should return [2, 3] for [1, 2, 3, 4]', function() {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('should return [] for ["Hello"]', function() {
+    assert.deepEqual(middle(["Hello"]), []);
+  });
+});
