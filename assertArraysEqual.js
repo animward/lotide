d80b8034dc4b575d5e9fc8f24d703e2f0148f935
EgoExperiments <egoexperiments@gmail.com>
2024-03-11 13:47:06 -0700

    removed assertEqual from test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,19 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = require('../assertEqual');
 
-const eqArrays = function (arr1, arr2) {
-  // Check if the arrays have the same length
-  if (arr1.length !== arr2.length) {
+const eqArrays = require('../eqArrays');
+
+// Check each element for equality
+for (let i = 0; i < arr1.length; i++) {
+  if (arr1[i] !== arr2[i]) {
     return false;
+    // Use break to exit the loop when an unequal pair is found
+    break;
   }
+}
 
-  // Check each element for equality
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-      // Use break to exit the loop when an unequal pair is found
-      break;
-    }
-  }
+return true;
 
-  return true;
-};
 
 const array1 = [1, 2, 3];
 const array2 = [1, 2, 3];
